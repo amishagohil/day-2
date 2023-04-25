2e4caec2f0e3bae29c7aaeef27c68e7ebb679b61
amishagohil <131762811+amishagohil@users.noreply.github.com>
2023-04-25 21:03:33 +0530

    lab work 19/04

---
+++ SUMS.C.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float a,b;
+	      a=34;
+	      b=89;
+	clrscr();
+	printf("\nthe number= %f",a+b);
+	printf("\nthe number= %f",a-b);
+	printf("\nthe number= %f",a*b);
+	printf("\nthe number= %f",a/b);
+	getch();
+
+
+}
